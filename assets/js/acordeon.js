24165b6a861114a1e7eaad4046ae0d151299c307
Luccas <luccasmelim@yahoo.com.br>
2023-07-30 23:56:59 -0300

    AtualizationAll-Prettier-RemoveImage and MediaQ

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,15 +1,15 @@
-const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
+const acordeonTriggers = document.querySelectorAll(".acordeon .trigger");
 
 acordeonTriggers.forEach((trigger) => {
-    trigger.addEventListener('click', (e) => {
-        const acordeon = trigger.parentElement
+  trigger.addEventListener("click", (e) => {
+    const acordeon = trigger.parentElement;
 
-        const isOpen = acordeon.classList.contains('open')
+    const isOpen = acordeon.classList.contains("open");
 
-        if (isOpen){
-            acordeon.classList.remove('open') 
-        } else {
-            acordeon.classList.add('open')
-        }
-    })
-})
+    if (isOpen) {
+      acordeon.classList.remove("open");
+    } else {
+      acordeon.classList.add("open");
+    }
+  });
+});
